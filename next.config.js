a7f605209f856d5a8a5ab0bccb589f8c21f04275
djsilcock <djsilcock@users.noreply.github.com>
2020-01-30 22:50:21 +0000

    Update

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@
 const path=require('path')
 require('dotenv').config()
 require('wavy/postinstall')
+secrets=require('./vault)
 module.exports = {
   webpack: function (config,{defaultLoaders,isServer,webpack}) {
+        if (isServer) config.plugins.push(new webpack.DefinePlugin(secrets))
 	config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/))
 	config.module.rules.push({
       test: /\.ifdef\.js/,
