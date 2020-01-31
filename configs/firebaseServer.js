c99be10087e15f50b7fa1aadf0681ff39b72fc49
djsilcock <dannysilcock@gmail.com>
2020-01-31 18:49:58 +0000

    typo

--- configs/firebaseServer.js
+++ configs/firebaseServer.js
@@ -1,7 +1,7 @@
-import firebase_admin from "firebase-admin"
+import firebase from "firebase-admin"
 
 const config= {
-    credential: process.env.FIREBASE ,
+    credential: firebase.credential.cert(process.env.FIREBASE) ,
     databaseURL: "https://qiexchange-223621.firebaseio.com"
   }
   
