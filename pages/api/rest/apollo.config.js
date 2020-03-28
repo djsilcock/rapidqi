49ebaff1775ac220dfd067877c1576f2de416858
djsilcock <dannysilcock@gmail.com>
2020-03-28 18:13:05 +0000

    tidying

--- pages/api/rest/apollo.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    client: {
-      includes: ["./src/**/*.js"],
-      excludes: ["**/__tests__/**"]
-    }
-  }
--- pages/api/rest/event/add.js
+++ pages/api/rest/event/add.js
@@ -1,16 +1,19 @@
-import addProject from '../../../../data-access/queries/addEvent.gql'
-import getClient from '../../../../data-access/apollo'
+import addEvent from "../../../../data-access/queries/addEvent.gql";
+import getClient from "../../../../data-access/apollo";
 
 export default async function AddEvent({ req, res }) {
-  const client = getClient({ req, res })
-  
-  if (req.method == 'POST') {
-    const project = req.body
+  const client = getClient({ req, res });
+
+  if (req.method == "POST") {
+    const project = req.body;
     try {
-      const result = await client.mutate({ mutation: addEvent, variables: { project })
-      res.status(200).json(result)
+      const result = await client.mutate({
+        mutation: addEvent,
+        variables: { project }
+      });
+      res.status(200).json(result);
     } catch (e) {
-      res.status(400).end(JSON.stringify(e))
+      res.status(400).end(JSON.stringify(e));
     }
   }
 }
